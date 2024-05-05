558c7ca096025f949b02a1d0db0ca95d4c7c6ce1
urielbang <uri32655@gmail.com>
2024-05-05 12:46:40 +0300

    mongo Sanitize

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const tourRouter = require('./routes/tour.routes');
 const userRouter = require('./routes/user.routes');
@@ -18,7 +20,7 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 // Limit requests from same API
 const limiter = rateLimit({
-  max: 3,
+  max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'To many request from this IP please try again in a hour',
 });
@@ -26,6 +28,11 @@ app.use('/api', limiter);
 
 // Body parser reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
