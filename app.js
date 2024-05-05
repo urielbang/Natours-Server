ec8db83e2230da22c983906d3a82ef0d6d3042e9
urielbang <uri32655@gmail.com>
2024-05-05 12:35:11 +0300

    helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const tourRouter = require('./routes/tour.routes');
 const userRouter = require('./routes/user.routes');
@@ -9,29 +10,36 @@ const { errorHandle } = require('./controllers/error.Controller');
 const app = express();
 
 //! 1)Global Middlewares
+//Set Security HTTP headers
+app.use(helmet());
 
+// Development logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
-
+// Limit requests from same API
 const limiter = rateLimit({
   max: 3,
   windowMs: 60 * 60 * 1000,
   message: 'To many request from this IP please try again in a hour',
 });
-
 app.use('/api', limiter);
-// app.use((req, res, next) => {
-//   req.requestTime = new Date().toISOString();
-//   console.log(req.headers);
-//   next();
-// });
 
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+// Body parser reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// Test Middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  //   console.log(req.headers);
+  next();
+});
 
 //! 2) Routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 //! ERROR HANDLING ,MIDLLEWARE
 app.use(errorHandle);
