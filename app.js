37cdf0ca78bbacc4384456fc953379250339d730
urielbang <uri32655@gmail.com>
2024-05-05 12:22:58 +0300

    rateLimit

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
-const AppError = require('./utils/appError');
+const rateLimit = require('express-rate-limit');
+
 const tourRouter = require('./routes/tour.routes');
 const userRouter = require('./routes/user.routes');
 const { errorHandle } = require('./controllers/error.Controller');
 
 const app = express();
 
-//! 1) Middalwares
+//! 1)Global Middlewares
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
+const limiter = rateLimit({
+  max: 3,
+  windowMs: 60 * 60 * 1000,
+  message: 'To many request from this IP please try again in a hour',
+});
+
+app.use('/api', limiter);
 // app.use((req, res, next) => {
 //   req.requestTime = new Date().toISOString();
 //   console.log(req.headers);
@@ -23,12 +32,6 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 //! 2) Routes
-
-// app.all('*', (req, res, next) => {
-//   next(new AppError(`Cannt find ${req.originalUrl} on this server!`));
-
-//   next(err);
-// });
 
 //! ERROR HANDLING ,MIDLLEWARE
 app.use(errorHandle);
