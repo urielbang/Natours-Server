5ebe2e6b4dfabed514b7d220612e8ad6bf3ee0ac
urielbang <uri32655@gmail.com>
2024-05-14 14:58:25 +0300

    start pug learn

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,8 +14,13 @@ const { errorHandle } = require('./controllers/error.Controller');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 //! 1)Global Middlewares
 //Set Security HTTP headers
+//Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 
 // Development logging
@@ -58,9 +64,6 @@ app.use(
   }),
 );
 
-//Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -69,6 +72,11 @@ app.use((req, res, next) => {
 });
 
 //! 2) Routes
+
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
